245a2f59b09ca48463b4ba93002237781779f743
Jackson <jackson.kelley.g@gmail.com>
2020-04-29 22:51:26 -0700

    fixed problems with the sendEmail function

--- exercise.js
+++ exercise.js
@@ -3,6 +3,7 @@
 //     console.log('Customer: ', customer);
 //     if (customer.isGold) {
 //         getTopMovies((movies) => {
+  
 //             console.log('Top movies: ', movies);
 //             sendEmail(customer.email, movies, () => {
 //                 console.log('Email sent...');
